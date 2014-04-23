402ff78540de76c4eb671be5f08fc6a26df905d8
Sawyer Hamann <sawyerhamann@hotmail.com>
2014-04-23 11:54:42 -0700

    I think I might've finished p3, need peer feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // My Scripts
 $(document).foundation();
 
-$('#myModal').foundation('reveal', 'open');
-$('#myModal').foundation('reveal', 'close');
+//$('#myModal').foundation('reveal', 'open');
+//$('#myModal').foundation('reveal', 'close');
 
 
